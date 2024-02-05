f7d2f5938e9a03beb2da2f221635cdfb134acf72
wordzseven <145131934+wordzseven@users.noreply.github.com>
2024-02-05 18:23:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,19 @@
 class Settings():
 
     def __init__(self):
-        self.screen_width = 1024
-        self.screen_height = 768
+        self.screen_width = 600
+        self.screen_height = 800
         self.bg_color = (255, 255, 255)
         self.ship_speed = 1.5
         self.ship_limit = 3
+
         #Параметры снаряда
-        self.bullet_speed = 1
-        self.bullet_width = 3
+        self.bullet_speed = 2
+        self.bullet_width = 1900
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
 
-        #Aliens
-        self.alien_speed = 1
+        #параметры пришельцев
+        self.alien_speed = 5.0
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
