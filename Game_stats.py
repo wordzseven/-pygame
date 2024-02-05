ec60e718405a3beed28d5b498bceaef77a651989
wordzseven <145131934+wordzseven@users.noreply.github.com>
2024-02-05 18:23:55 +0300

    Add files via upload

---
+++ Game_stats.py
@@ -0,0 +1,8 @@
+class Game_stats():
+    def __init__(self, ai_game):
+        self.settings = ai_game.Settings
+        self.reset_stats()
+        self.game_active = False
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
